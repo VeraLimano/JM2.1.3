67a05078b03518ecea7d1f11e1792c61686c37c7
VeraLimano <69342940+VeraLimano@users.noreply.github.com>
2020-11-08 11:08:14 +0300

    first commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,10 +2,17 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    List<Long> list = new ArrayList<>();
+    static long time = 0;
 
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -15,6 +22,7 @@ public class Application {
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
+
     }
 
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,19 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+    AnimalsCage animalsCage;
 
     private Long nanoTime = System.nanoTime();
+
 
     public Long getTime() {
         return nanoTime;
     }
+
+
+
 }
